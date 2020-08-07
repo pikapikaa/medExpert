d35147397f862488a03eb122c7669b9968878d43
pikapikaa <gorod3012@gmail.com>
2020-08-07 17:08:54 +0800

    экран проверки

--- medExpert/medExpert/ViewModels/Audits/AuditOperationRecordViewModel.cs
+++ medExpert/medExpert/ViewModels/Audits/AuditOperationRecordViewModel.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using medExpert.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Xamarin.Forms;
 
 namespace medExpert.ViewModels.Audits
 {
-    public class AuditOperationRecordViewModel
+    public class AuditOperationRecordViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<StructuralUnit> structuralUnits;
+        public ObservableCollection<StructuralUnit> StructuralUnits
+        {
+            get { return structuralUnits; }
+            set
+            {
+                structuralUnits = value;
+                OnPropertyChanged();
+            }
+        }
         public string Num { get; set; }
 
         public string CompanyName { get; set; }
@@ -36,6 +50,13 @@ namespace medExpert.ViewModels.Audits
             GenerateSource();
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
         private void GenerateSource()
         {
             Num = "1";
@@ -44,6 +65,33 @@ namespace medExpert.ViewModels.Audits
             TitleIndicator = "Отпуск и реализация лекарственных препаратов для медицинского применения в аптеке производственной (Приложение 23)";
             PeriodDateIn = new DateTime(2020, 5, 5);
             PeriodDateOut = new DateTime(2020, 5, 8);
+
+            var _listOfItems = new DataStructuralUnitFactory().GetData();
+            StructuralUnits = new ObservableCollection<StructuralUnit>(_listOfItems);
+        }
+    }
+
+    internal class DataStructuralUnitFactory
+    {
+        public List<StructuralUnit> GetData()
+        {
+            return new List<StructuralUnit>()
+            {
+               new StructuralUnit(){Id=1,Name = "Республиканская клиническая больница скорой медицинской помощи им. В.В. Ангапова"},
+               new StructuralUnit(){Id=2,Name = "Детская республиканская клиническая больница"},
+               new StructuralUnit(){Id=3,Name = "Республиканская клиническая инфекционная больница"},
+               new StructuralUnit(){Id=4, Name = "РЖД-МЕДИЦИНА, клиническая больница"},
+               new StructuralUnit(){Id=5,Name = "Городская поликлиника №5, Городская больница №5"},
+               new StructuralUnit(){Id=6, Name = "Городская поликлиника №1, Городская больница №1"},
+               new StructuralUnit(){Id=7,Name = "Городская поликлиника №2, Городская больница №2"},
+               new StructuralUnit(){Id=8, Name = "Городская поликлиника №3, Городская больница №3"},
+               new StructuralUnit(){Id=9,Name = "Городская поликлиника №4, Городская больница №4"},
+               new StructuralUnit(){Id=10, Name = "Городская поликлиника №6, Городская больница №6"},
+               new StructuralUnit(){Id=11,Name = "Республиканский перинатальный центр Министерства здравоохранения Республики Бурятия"},
+               new StructuralUnit(){Id=12, Name = "Республиканская клиническая больница им. Н.А. Семашко"},
+               new StructuralUnit(){Id=13,Name = "Иволгинская центральная районная больница"},
+               new StructuralUnit(){Id=14, Name = "Ильинская участковая больница"},
+            };
         }
     }
 }
