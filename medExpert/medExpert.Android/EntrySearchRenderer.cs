a900a4797e9e113973d5819a77ad7a316c7635fb
pikapikaa <gorod3012@gmail.com>
2020-08-08 22:48:53 +0800

    Добавил EntryRenderer

---
+++ medExpert/medExpert.Android/EntrySearchRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using medExpert.Droid;
+using medExpert.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntryRenderer), typeof(EntrySearchRenderer))]
+namespace medExpert.Droid
+{
+    public class EntrySearchRenderer : EntryRenderer
+    {
+        public EntrySearchRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null || e.NewElement == null) return;
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                Control.BackgroundTintList = ColorStateList.ValueOf(Xamarin.Forms.Color.White.ToAndroid());
+            }
+            else
+            {
+                Control.Background.SetColorFilter(Xamarin.Forms.Color.White.ToAndroid(), PorterDuff.Mode.SrcAtop);
+            }
+        }
+    }
+}
---
+++ medExpert/medExpert.iOS/EntrySearchRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using medExpert.iOS;
+using medExpert.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntryRenderer), typeof(EntrySearchRenderer))]
+namespace medExpert.iOS
+{
+    public class EntrySearchRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.BorderStyle = UIKit.UITextBorderStyle.None;
+            }
+        }
+    }
+}
