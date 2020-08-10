d7f5f21cb40564d5d93b926541b4a5cdd467b495
pikapikaa <gorod3012@gmail.com>
2020-08-10 11:43:49 +0800

    верстка экрана новое стр. подр-е

--- medExpert/medExpert/ViewModels/Audits/NewStructuralUnitViewModel.cs
+++ medExpert/medExpert/ViewModels/Audits/NewStructuralUnitViewModel.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace medExpert.ViewModels.Audits
 {
-    public class NewStructuralUnitViewModel
+    public class NewStructuralUnitViewModel : INotifyPropertyChanged
     {
         public INavigation Navigation;
+        private string _nameStructuralUnit = "";
+
+        public string NameStructuralUnit
+        {
+            get { return _nameStructuralUnit; }
+            set
+            {
+                _nameStructuralUnit = value;
+                OnPropertyChanged();
+            }
+        }
 
         /// <summary>
         /// Команда для закрытия текущего экрана
@@ -17,5 +30,31 @@ namespace medExpert.ViewModels.Audits
         {
             await Navigation.PopModalAsync();
         });
+
+        /// <summary>
+        /// Команда создания нового подразделения
+        /// </summary>
+        public ICommand AddNewStructuralUnitCommand => new Command(async () =>
+        {
+            await Navigation.PopModalAsync();
+        });
+
+        public ICommand SearchTextChangedCommand => new Command(async () =>
+        {
+            if (string.IsNullOrWhiteSpace(NameStructuralUnit))
+            {
+
+            }
+            else
+            {
+
+            }
+        });
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
