5c8810907d8ec14a071b39883278e73177741217
pikapikaa <gorod3012@gmail.com>
2020-09-16 19:06:02 +0800

    добавил renderer для Editor, чтобы убрать нижнюю линию

---
+++ medExpert/medExpert.Android/EditorTaskRenderer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using medExpert.Droid;
+using medExpert.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEditorRenderer), typeof(EditorTaskRenderer))]
+namespace medExpert.Droid
+{
+    public class EditorTaskRenderer : EditorRenderer
+    {
+        public EditorTaskRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null || e.NewElement == null) return;
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                Control.BackgroundTintList = ColorStateList.ValueOf(Xamarin.Forms.Color.Transparent.ToAndroid());
+            }
+            else
+            {
+                Control.Background.SetColorFilter(Xamarin.Forms.Color.Transparent.ToAndroid(), PorterDuff.Mode.SrcAtop);
+            }
+        }
+    }
+}
---
+++ medExpert/medExpert.iOS/EditorTaskRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using medExpert.iOS;
+using medExpert.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEditorRenderer), typeof(EditorTaskRenderer))]
+namespace medExpert.iOS
+{
+    public class EditorTaskRenderer : EditorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+            //???? нужно добавить обработчик для удаления линии
+        }
+    }
+}
