93bc73080be9f59ef2bb830603e99bcea28ab44d
pikapikaa <gorod3012@gmail.com>
2020-09-24 15:17:11 +0800

    добавил конвертер TaskPriorityToColorConverter к статусам задач

---
+++ medExpert/medExpert/Converters/TaskPriorityToColorConverter.cs
@@ -0,0 +1,37 @@
+﻿using medExpert.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace medExpert.Converters
+{
+    class TaskPriorityToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            AuditOperationStatus flag = (AuditOperationStatus)value;
+            switch (flag)
+            {
+                case AuditOperationStatus.Created:
+                    return "#3C63E7";
+                case AuditOperationStatus.Executed:
+                    return "#834EA0";
+                case AuditOperationStatus.Running:
+                    return "#3CBB78";
+                case AuditOperationStatus.Signed:
+                    return "#834EA0";
+                case AuditOperationStatus.Signing:
+                    return "#834EA0";
+                default:
+                    return "white";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
