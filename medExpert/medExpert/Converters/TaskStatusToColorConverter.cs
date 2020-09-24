e9fd0af696f369a0dc91b714a258413867e67a55
pikapikaa <gorod3012@gmail.com>
2020-09-24 15:42:57 +0800

    добавил конвертер  для статуса задач TaskStatusToColorConverter

---
+++ medExpert/medExpert/Converters/TaskStatusToColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using medExpert.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace medExpert.Converters
+{
+    public class TaskStatusToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TaskStatus flag = (TaskStatus)value;
+            switch (flag)
+            {
+                case TaskStatus.New:
+                    return "#3960E2";
+                case TaskStatus.Assigned:
+                    return "#3CBB78";
+                case TaskStatus.InProgress:
+                    return "#3CBB78";
+                case TaskStatus.Executed:
+                    return "#834EA0";
+                default:
+                    return "white";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
